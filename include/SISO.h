4461e5d7f7f3164678b298d18f9a7b85cdc307ab
FRYDRYCHOWSKI Valentin <frydrychowskiv@gmail.com>
2020-04-02 18:24:17 +0200

    added num check feature

--- include/SISO.h
+++ include/SISO.h
@@ -1,13 +1,14 @@
 #ifndef __SISOH__
 #define __SISOH__
 
-
-void read_int(int* nb);
-    /*
+/*
     get safely an int
+    return 1 if stdin is not a num
     */
+int read_int(int* nb);
+    
 
-void read_string(char* string);
+void read_string(char* string, int size_str);
     /*
     get safely a string
     */
--- src/SISO.c
+++ src/SISO.c
@@ -6,43 +6,49 @@
 
 void cleanbuff(){
     int c = 0;
-    while (c != 0 && c != EOF){
+    while (c != '\n' && c != EOF){
         c = getchar();
     }
 }
-void read_int(int* nb){
-    //set the maximu size to write an integer + '\n'
+
+
+int read_int(int* nb){
+    //set the maximum size to write an integer + '\n'
+    int bool = 1;
     char* temp = malloc(sizeof(char)*9);
     if (temp == NULL){
         perror("ERR MALLOC\n");
         exit(1);
     }
 
-    fgets(temp, 9, stdin);
-    if (temp == NULL){
-        perror("ERR fgets()\n");
-        exit(1);
+    //ASCII check
+    read_string(temp,sizeof(char)*9);
+    for(int i=0; i<strlen(temp); i++){
+        if (((int)temp[i] < 48 || (int)temp[i] > 57) && (int)temp[i] != 45){
+            bool = 0;
+        }
     }
-    cleanbuff();
-    //replace enter char by end of string char
-    char* enter = NULL;
-    enter = strchr(temp, '\n');
-    if (enter != NULL) *enter = '\0';
+    if (bool== 0) return 1;    
 
+    //int cast
     *nb = atoi(temp);
     free(temp);
+    return 0;
 }
 
 //assume string has already been allocate
-void read_string(char* string){
-    fgets(string, strlen(string), stdin);
+void read_string(char* string, int size_str){
+    fgets(string, size_str, stdin);
     if (string == NULL){
         perror("ERR fgets()\n");
+        cleanbuff();
         exit(1);
     }
-    cleanbuff();
     //replace enter char by end of string char
     char* enter = NULL;
     enter = strchr(string, '\n');
-    if (enter != NULL) *enter = '\0';
+    if (enter == NULL){
+        cleanbuff();
+    }
+    *enter = '\0';
 }
