852a1dad9fe4bc45504f06c3ed5ec56ee8beb040
FRYDRYCHOWSKI Valentin <frydrychowskiv@gmail.com>
2020-04-02 18:42:01 +0200

    a bit of commenting

--- include/SISO.h
+++ include/SISO.h
@@ -1,16 +1,22 @@
 #ifndef __SISOH__
 #define __SISOH__
 
+/* 
+    clean the buffer
+*/
+void cleanbuff();
+
 /*
     get safely an int
     return 1 if stdin is not a num
     */
 int read_int(int* nb);
     
-
-void read_string(char* string, int size_str);
-    /*
+/*
     get safely a string
+    assumed that string has already been allocate
     */
+void read_string(char* string, int size_str);
+    
 
 #endif
