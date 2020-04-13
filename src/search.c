291ce304b75e6982244ab7de7aec1274f104c94c
FRYDRYCHOWSKI Valentin <frydrychowskiv@gmail.com>
2020-04-14 01:48:27 +0200

    search_date() rewritted

--- src/search.c
+++ src/search.c
@@ -197,40 +197,88 @@ char** search_author(char* author){
 }
 
 char** search_date(char* date){
+        //initialization of index
     char** tab = get_gen_table("data/object/obj.json", "obj");
-    //first loop to find the size of index
+    char** index = (char**)malloc(sizeof(char*));
+    if (index == NULL){
+        perror("malloc index in search_title failed");
+        exit(EXIT_FAILURE);
+    }
+    index[0] = (char*)malloc(sizeof(char)*IDSIZE);
+    if (index[0] == NULL){
+        perror("malloc index[0] in search_title failed");
+        exit(EXIT_FAILURE);
+    }
     int size=0;
+    char* csize = (char*)malloc(sizeof(char)*5);
+    if (csize == NULL){
+        perror("malloc csize in search_title failed");
+        exit(EXIT_FAILURE);
+    }
+    sprintf(csize, "%d", size);
+    strcpy(index[0], csize);
+
+    //retrieving the book's date that contains name
     char* d;
     for(int i = 1; i<=get_table_size(tab); i++){
         d = get_date(tab[i]);
-        if (strstr(d, date) != NULL){
+        if (strstr(d, date) != NULL || cstring_cmp(date, "")==0){
             size++;
-        }
-        free(d);
-    }
-    
-    char** index = (char**)malloc(sizeof(char*)*(size+1)*2);
-    index[0] = (char*)malloc(sizeof(char)*IDSIZE);
-    char* csize = (char*)malloc(sizeof(char)*5);
-    sprintf(csize, "%d", size*2);
-    strcpy(index[0], csize);
-    //for each date that match name, we put the date and the id in the table
-    size=1;
-    for(int i = 1; i<=get_table_size(tab) && size<(get_table_size(index)); i++){
-        d = get_date(tab[i]);
-        if (strstr(d, date) != NULL){
-            index[size] = (char*)malloc(sizeof(char)*IDSIZE);
-            strcpy(index[size], tab[i]);
-            size++;
-            index[size] = (char*)malloc(sizeof(char)*NAMESIZE);
+            //rezise index
+            index = (char**)realloc(index, sizeof(char*)*(size+2));
+            if (index == NULL){
+                perror("realloc index in search_title failed");
+                exit(EXIT_FAILURE);
+            }
+            sprintf(csize, "%d", size);
+            strcpy(index[0], csize);
+            index[size] = (char*)malloc(sizeof(char)*(IDSIZE + NAMESIZE +1));
+            if (index[size] == NULL){
+                perror("malloc index[size] in search_title failed");
+                exit(EXIT_FAILURE);
+            }
             strcpy(index[size], d);
-            size++; 
+            //adding a separator token
+            strcat(index[size], "¤");
+            strcat(index[size], tab[i]);
         }
         free(d);
     }
+    //sorting the index
+    qsort(index+1, get_table_size(index), sizeof(char*), cstring_cmp);
+    
+    
+
+    //array initialazing
+    size = get_table_size(tab)*2;
+    char** sep_index = malloc(sizeof(char*)*(size+1));
+    sprintf(csize, "%d", size);
+    sep_index[0] = (char*)malloc(sizeof(char)*IDSIZE);
+    strcpy(sep_index[0], csize);
+    
+    //separate authors and id
+    char* strToken;
+    for(int i = 1; i<=get_table_size(sep_index); i=i+2){
+        strToken = strtok(index[i/2+1], "¤");
+        sep_index[i] = (char*)malloc(sizeof(char)*NAMESIZE);
+        if (sep_index[i] == NULL){
+            perror("sep_index[i] in search_title failed");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(sep_index[i], strToken);
+        sep_index[i+1] = (char*)malloc(sizeof(char)*IDSIZE);
+        if (sep_index[i+1] == NULL){
+            perror("sep_index[i+1] in search_title failed");
+            exit(EXIT_FAILURE);
+        }
+        strToken = strtok(NULL, "¤");
+        strcpy(sep_index[i+1], strToken);
+    }
+
     free_table(tab);
+    free_table(index);
     free(csize);
-    return index;
+    return sep_index;
 }
 
 void search(){
