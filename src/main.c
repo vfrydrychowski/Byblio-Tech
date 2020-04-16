beb6fb24f3c5739e4e513f63f4dd7012f741f7aa
FRYDRYCHOWSKI Valentin <frydrychowskiv@gmail.com>
2020-04-16 19:21:10 +0200

    home menu

--- src/main.c
+++ src/main.c
@@ -9,10 +9,51 @@
 #include "../include/SISO.h"
 #include "../include/user.h"
 
+void Menu(){
+    int x = 0;
+    int pos = -1;
+    while(pos == -1){
+        cleanscr();
+        printf(" ----------------------------------------------------------------------\n");
+        printf("|                       Welcome to Byblio-Tech!!                       |\n");
+        printf(" ----------------------------------------------------------------------\n");
+        printf("|                         1 : Login                                    |\n");
+        printf("|                                                                      |\n");
+        printf("|                         2 : Register                                 |\n");
+        printf("|                                                                      |\n");
+        printf("|                         0 : Quit                                     |\n");
+        printf(" ----------------------------------------------------------------------\n");
+        printf(" Choice : ");
+        read_int(&x);
+        switch(x){
+            case 1 :
+
+            break;
+
+            case 2 :
+
+            break;
+            
+            case 0 :
+                pos = 0;
+            break;
+
+            default :
+            break;
+
+
+        }
+    }
+    
+
+
+
+}
+
 
 int main(int argc, char const *argv[])
 {
     printf("rose are red\nviolets are blue\nthis main is on the edge\nof a new and wonderfull youth\n");
-    search();
+    Menu();
     return 0;
 }
