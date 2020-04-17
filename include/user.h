bc5a931d5c3f65ecf13a233e9ce49ab60ce50ede
tiltal <tiltal@linux.home>
2020-04-17 14:09:35 +0200

    user.c commented

--- include/user.h
+++ include/user.h
@@ -28,13 +28,11 @@ void uset_grade(int grade, User util);
 void uset_brw(char** brw, User util);
 void uset_possession(char** possession, User util);
 
-int exist_user(char* id);
 	/*
 	if the user exist return 0
 	else return 1
 	*/
-int crea_user(User* util, char* id, char* forename, char* name, char* mail, int grade, char* Pwd);
-
+int exist_user(char* id);
 	/*
 	charge the User struct util and fill it with the arguments
 	create a Json for util
@@ -43,16 +41,16 @@ int crea_user(User* util, char* id, char* forename, char* name, char* mail, int 
 	return 2 if an other user has alreary this mail
 	return 3 if an other user has already this id
 	*/
-int possession_borrow(User user);
+int crea_user(User* util, char* id, char* forename, char* name, char* mail, int grade, char* Pwd);
 	/*
 	return the number of possession witch has a borrower
 	*/
-int exist_in_list(char* substring,char* listname);
+int possession_borrow(User user);
 	/*
-	return 0 if there is an occurence of subtring in the list
-	else return 1
+	return 0 if all the possession of user are not borrow
+	else return the number of possession witch have a borrower
 	*/
-int suppr_us(User user);
+int exist_in_list(char* substring,char* listname);
 	/*
 	suppr all object file of id possessions
 	return back all the borrowing of id
@@ -60,7 +58,7 @@ int suppr_us(User user);
 	return 0 if all goes well
 	return 1 if one of the possession of user has been borrow
 	*/
-int ban(char* id,User user);
+int suppr_us(User user);
 	/*
 	suppr all object file of id possessions
 	return back all the borrowing of id
@@ -69,15 +67,19 @@ int ban(char* id,User user);
 	return 1 if the id do not exist
 	return 2 if user has higher or the same grade than id  
 	*/
-int login(User* user, char* id, char* pwd);
+int ban(char* id,User user);
 	/*
 	check the user identity and fill util with infos in user’s json and return 0
 	return 1 if the user do not exist
 	return 2 if pwd (after being crypted) don't match the one in the json
 	*/
-User charge_user(char* id);
+int login(User* user, char* id, char* pwd);
 	/*
 	create au user struc whith data of the correponding json
+	*/
+User charge_user(char* id);
+	/*
+	free the struct user
 	*/
 void logout(User user);
 	/*
@@ -85,60 +87,59 @@ void logout(User user);
 	*/
 void free_user(User user);
 	/*
-	free the struct user
-	*/
-int exist_in_table(char* chaine,char** table);
-	/*
 	return the number of occurence of chaine in table
 	*/
-int borrowing(User util, char* idObject);
+int exist_in_table(char* chaine,char** table);
 	/*
 	update status of the object, add it of the current user’s borrowing list and return 0
 	return 1 if the object is already borrow
 	*/
-int return_back(char* idObject, User util);
+int borrowing(User util, char* idObject);
 	/*
 	update status of the object, remove it of the current user’s borrowing list and return 0
 	return 1 if user hasn't borrow the object 
 	*/
-void return_back_all(User util);
+int return_back(char* idObject, User util);
 	/*
 	update status of all objects, remove it of the current user’s borrowing list
 	*/
-void suppr_all_possession(User user);
+void return_back_all(User util);
 	/*
 	update status of the object and remove it of the current user’s borrowing list
 	*/
-void add_possession(User user, char* name, int pagenb, char* author, int date, char* kind);
+void suppr_all_possession(User user);
 	/*
 	add idObject to the user’s possesion list 
 	*/
-int suppr_possession(char* idObject, User user);
+void add_possession(User user, char* name, int pagenb, char* author, int date, char* kind);
 	/*
 	remove idObject of the user’s possesion list and return 0 
 	if Object is borrow by an other user return 1
 	if the user don't possess the Object return 2
 	*/
-
-void encrypt(char* pwd, char* crypwd);
+int suppr_possession(char* idObject, User user);
 	/*
 	encrypt pwd and put it in crywd
 	*/
-int new_pwd(User user, char* pwd, char* nv_pwd);
+void encrypt(char* pwd, char* crypwd);
 	/*
 	change the crypted password in the user's json
 	return 0 if everything goes well
 	return 1 if pwd do not match whith the password in the json
 	*/
-int new_username(User user,char* new_username);
+int new_pwd(User user, char* pwd, char* nv_pwd);
 	/*
-
+	change the id in struct user
+	change the json of the user
+	change the borrower in his borrow's json
+	change the owner in his possession json
 	return 0 if everything goes well
 	return 1 if an other user as already new_usermane as id
 	*/
-char** duplicate_table(char** tab);
+int new_username(User user,char* new_username);
 	/*
 	dupilcate tab of ID using a malloc
 	*/
+char** duplicate_table(char** tab);
 
 #endif
